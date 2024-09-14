7ed4a3d8366727c12756bd2a014976d6139b4077
clienterr <clienterr@clienterr.com>
2024-09-14 13:20:19 +0545

    Public release

---
+++ js/commands.js
@@ -0,0 +1,72 @@
+var youtube = "https://www.youtube.com/clienterr/";
+var password = "clienterrverse";
+var instagram = "https://www.instagram.com/clienterr_/";
+var github = "https://github.com/clienterr/";
+var email = 'mailto:clienterr@clienterr.com';
+
+whois = [
+    "<br>",
+    "Hey, I'm Clienterr!👋",
+    "I'm an Undergraduate Student, who builds engaging websites like this one",
+    "and makes YouTube videos.",
+    "<br>"
+];
+
+whoami = [
+    "<br>",
+    "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+    "<br>"
+];
+
+social = [
+    "<br>",
+    'youtube        <a href="' + youtube + '" target="_blank">youtube/clienterr' + "</a>",
+    'instagram      <a href="' + instagram + '" target="_blank">instagram/clienterr_' + '</a>',
+    'github         <a href="' + github + '" target="_blank">github/clienterr' + "</a>",
+    "<br>"
+];
+
+secret = [
+    "<br>",
+    '<span class="command">sudo</span>           Only use if you\'re admin',
+    "<br>"
+];
+
+projects = [
+    "<br>",
+    "Still curating... most projects are offline, on GitHub, or confidential.",
+    "<br>"
+];
+
+help = [
+    "<br>",
+    '<span class="command">whois</span>          Who is Clienterr?',
+    '<span class="command">whoami</span>         Who are you?',
+    '<span class="command">video</span>          View YouTube videos',
+    '<span class="command">social</span>         Display social networks',
+    '<span class="command">secret</span>         Find the password',
+    '<span class="command">projects</span>       View coding projects',
+    '<span class="command">history</span>        View command history',
+    '<span class="command">help</span>           You obviously already know what this does',
+    '<span class="command">email</span>          Do not email me',
+    '<span class="command">clear</span>          Clear terminal',
+    '<span class="command">banner</span>         Display the header',
+    "<br>",
+];
+
+banner = [
+    '<span class="index">Clienterrverse (CV) Not A Corporation. All knights reserved.</span>',
+    "----------------------------------------------------------------------------------------------------------------",
+    
+ "░▒▓██████▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓████████▓▒░▒▓███████▓▒░▒▓████████▓▒░▒▓████████▓▒░▒▓███████▓▒░░▒▓███████▓▒░  ",
+"░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ",
+"░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ",
+"░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░▒▓██████▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░   ░▒▓██████▓▒░ ░▒▓███████▓▒░░▒▓███████▓▒░ ", 
+"░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ",
+"░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ",
+ "░▒▓██████▓▒░░▒▓████████▓▒░▒▓█▓▒░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░   ░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ",
+                                                                                                               
+    "----------------------------------------------------------------------------------------------------------------",
+    '<span class="color2">Welcome to my interactive web terminal.</span>',
+    "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
